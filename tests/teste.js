dcdf035d4c60510583617c6ba04b8694d39ac72e
Lucas <lucas.alt40@gmail.com>
2021-01-30 13:12:21 -0300

    testes

---
+++ tests/teste.js
@@ -0,0 +1,6 @@
+document.getElementById("square").onclick = function() {clica()};
+
+function clica() {
+  document.getElementById("square").style.width = "500px";
+  document.getElementById("ret1").style.width = "500px";
+}
