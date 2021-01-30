64f3b8561402a3db4e0c9614364779102003cc67
Lucas <lucas.alt40@gmail.com>
2021-01-30 13:21:13 -0300

    testes

--- tests/teste.js
+++ tests/teste.js
@@ -1,6 +1,14 @@
-document.getElementById("square").onclick = function() {clica()};
+document.getElementById("laco").onclick = function() {clica()};
 
 function clica() {
-  document.getElementById("square").style.width = "500px";
-  document.getElementById("ret1").style.width = "500px";
+  document.getElementById("square").style.width = "876px";
+  document.getElementById("square").style.height = "395px";
+
+  document.getElementById("ret1").style.width = "876px";
+  document.getElementById("ret1").style.height = "44px";
+
+  document.getElementById("laco").style.width = "341px";
+  document.getElementById("laco").style.height = "317px";
+
+  document.getElementById("nome-produto").style.marginLeft = "350px";
 }
